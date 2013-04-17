d09420519306cf4bd745946a01b99314fdd50ccc
Abigail Watson <awatson1978@gmail.com>
2013-04-17 12:30:34 -0400

    updated to cordova 2.6.0

--- package.js
+++ package.js
@@ -4,5 +4,5 @@ Package.describe({
 
 Package.on_use(function (api) {
     api.add_files('app.initialization.js', 'client');
-    api.add_files('cordova-2.5.0rc1.js', 'client');
+    api.add_files('cordova-2.6.0.js', 'client');
 });

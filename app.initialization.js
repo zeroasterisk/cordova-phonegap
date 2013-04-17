1170becbb596cb46aa25739d410be1d40abc5e5e
Abigail Watson <awatson1978@gmail.com>
2013-04-17 12:33:27 -0400

    updated app.initialization.js with code from zeroasterix, removed var, etc

--- app.initialization.js
+++ app.initialization.js
@@ -1,8 +1,8 @@
 
 //-------------------------------------------------------
-// CORDOVOA PHONE GAP
+// Cordova App Object Literal
 
-var app = {
+app = {
     // Application Constructor
     initialize: function() {
         this.bindEvents();
@@ -13,6 +13,30 @@ var app = {
     // 'load', 'deviceready', 'offline', and 'online'.
     bindEvents: function() {
         document.addEventListener('deviceready', this.onDeviceReady, false);
+
+        // This is an event that fires when a Cordova application is retrieved from the background.
+        document.addEventListener("resume", function() {
+            // http://docs.meteor.com/#meteor_reconnect
+            // Force an immediate reconnection attempt if the client is not connected to the server.
+            // This method does nothing if the client is already connected.
+            Meteor.reconnect();
+            Meteor.resume();
+        });
+        // when an app goes into the background
+        document.addEventListener("Pause", function() {
+            Cookie.set('LastPage', Meteor.Router.page());
+        });
+        // when an app drops 'offline'
+        document.addEventListener("offline", function() {
+            if (Meteor.Router.page() != 'offline' && Meteor.Router.page() != 'loading') {
+                Cookie.set('LastPage', Meteor.Router.page());
+                Meteor.Router.to('/offline');
+            }
+        });
+        // when an app comes 'online'
+        document.addEventListener("online", function() {
+            Meteor.resume();
+        });
     },
     // deviceready Event Handler
     //
@@ -34,3 +58,25 @@ var app = {
     }
 };
 
+
+//-------------------------------------------------------
+// METEOR
+
+
+// resume functionality, common (used in offline.js as well)
+Meteor.resume = function() {
+    if (Meteor.status().status != 'connected') {
+        return false;
+    }
+    if (Meteor.Router.page() != 'offline' && Meteor.Router.page() != 'loading') {
+        return true;
+    }
+    var LastPage = Cookie.get('LastPage');
+    if (_.isString(LastPage) && LastPage.length && LastPage != 'loading') {
+        console.log('resumed to: (LastPage)', '/' + LastPage);
+        Meteor.Router.to('/' + LastPage);
+        return true;
+    }
+    Meteor.Router.to('/');
+    return true;
+};
--- package.js
+++ package.js
@@ -3,6 +3,7 @@ Package.describe({
 });
 
 Package.on_use(function (api) {
+    api.add_files('cordova.css', 'client');
     api.add_files('app.initialization.js', 'client');
     api.add_files('cordova-2.6.0.js', 'client');
 });
